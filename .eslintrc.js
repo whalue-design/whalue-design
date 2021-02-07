74a3f1c2bc483e415d2f24208cabeb2fb42c3e3a
cnguu <www@cnguu.cn>
2021-02-07 18:58:01 +0800

    build: Adding Development Dependencies

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  parser: "vue-eslint-parser",
+  parserOptions: {
+    parser: "@typescript-eslint/parser",
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true,
+      tsx: true,
+    },
+  },
+  env: {
+    browser: true,
+    node: true,
+  },
+  plugins: ["@typescript-eslint"],
+  extends: [
+    "plugin:@typescript-eslint/recommended",
+    "plugin:vue/vue3-recommended",
+    "@vue/prettier",
+    "@vue/prettier/@typescript-eslint",
+  ],
+  rules: {},
+};
